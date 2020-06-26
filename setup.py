b9278440ee2272da745d432b9ceddd285cb6187a
Yuval Rosen <yuv.rosen@gmail.com>
2020-06-26 15:43:08 +0300

    replace setup.py with setup.bat

--- setup.py
+++
@@ -1,7 +0,0 @@
-import os
-if __name__ == "__main__":
-    print(os.path.dirname(os.path.abspath(__file__)))
-    if (input("Add this directory to path? (y/n) ").lower() == "y"):
-        os.environ["PATH"] += ";" + os.path.dirname(os.path.abspath(__file__))
-    else:
-        print("Operation canceled by user.")
