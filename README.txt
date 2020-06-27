6ca38d5febeb226621614941ed701d9866ffbf1e
Yuval Rosen <yuv.rosen@gmail.com>
2020-06-27 15:34:17 +0300

    update

---
+++ README.txt
@@ -0,0 +1,19 @@
+# pytov
+Python but tov (good).
+
+## Getting Started
+Run the setup.bat file.
+
+## Features
+* Curly braces
+* No identation errors
+* Long and short comments (`/**/`, `//`)
+* Switch statement
+* Compiles to python
+
+## Requirements
+* python 3
+
+## How to use
+To run files, **after running setup.bat (see Get Started)**, simply run in the command line `$ pytov path_to_file`.
+If you want to also save the compiled python file use (`$ pytov path_to_file -py`).
--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
-from distutils.core import setup
-setup(
-  name = 'pytov',         # How you named your package folder (MyLib)
-  packages = ['pytov'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'Yuval Rosen',                   # Type in your name
-  author_email = 'yuv.rosen@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/Yuvix25/pytov',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Yuvix25/pytov/archive/v0.1.tar.gz',    # I explain this later on
-  keywords = ['syntax', 'curlybraces', 'curly braces', 'switch', 'long comments', 'multiline comments'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytov", # Replace with your own username
+    version="0.2",
+    license='MIT',
+    author="Yuval Rosen",
+    author_email="yuv.rosen@gmail.com",
+    description="Write python with C like syntax",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Yuvix25/pytov",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[            # I get to this in a second
           'importlib'
       ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-  ],
 )
